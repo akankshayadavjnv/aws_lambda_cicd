e0b4c99b19e2f9477d66c00d10537ea089e7cc5a
akanksha-yadav <akankshayadav912@gmail.com>
2024-03-21 09:06:16 +0530

    remove pandas dependency and change buildspec accordingly

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,6 @@
+import json
+
 import boto3
-import pandas as pd
 
 s3_client = boto3.client("s3")
 sns_client = boto3.client("sns")
@@ -10,20 +11,20 @@ def lambda_handler(event, context):
     try:
         bucket_name = event["Records"][0]["s3"]["bucket"]["name"]
         s3_file_key = event["Records"][0]["s3"]["object"]["key"]
-        res = s3_client.get_object(Bucket=bucket_name, Key=s3_file_key)
-        data = pd.read_json(res["Body"], lines=True)
-        print("data", data)
-        filter_data = data[data["status"] == "delivered"]
-        print("filter_data", filter_data)
-        destination_bucket = "doordash-target-zn12"
+        response = s3_client.get_object(Bucket=bucket_name, Key=s3_file_key)
+        lines = response["Body"].read().decode("utf-8").split("\n")
+        op = []
+        for line in lines:
+            item = json.loads(line)
+            if item.get("status") == "delivered":
+                op.append(line)
+        op_str = "\n".join(op)
 
-        buffer = filter_data.to_json(
-            lines=True,
-            orient="records",
-        )
-        print("buffer", buffer)
         print("writing to s3")
-        s3_client.put_object(Bucket=destination_bucket, Key=s3_file_key, Body=buffer)
+        destination_bucket = "doordash-target-zn12"
+        s3_client.put_object(
+            Bucket=destination_bucket, Key=s3_file_key, Body=op_str.encode("utf-8")
+        )
         sub = "Successfull: data processing"
         message = "input s3 file {} {} has been processed succesfully".format(
             bucket_name, s3_file_key
